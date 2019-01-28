844678db8c73f4dbe85373f003f893214acd1636
CyrilenBlu <lukepetzerint2@gmail.com>
2019-01-28 15:02:02 +0200

    Added method and UserController, todo fix.

--- src/main/java/blue/springframework/springrest/services/ApiService.java
+++ src/main/java/blue/springframework/springrest/services/ApiService.java
@@ -1,9 +1,12 @@
 package blue.springframework.springrest.services;
 
 import blue.springframework.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
     List<User> getUsers(Integer limit);
+    Flux<User> getUsersReactive(Mono<Integer> limit);
 }
--- src/main/java/blue/springframework/springrest/services/ApiServiceImpl.java
+++ src/main/java/blue/springframework/springrest/services/ApiServiceImpl.java
@@ -2,8 +2,12 @@ package blue.springframework.springrest.services;
 
 import blue.springframework.api.domain.User;
 import blue.springframework.api.domain.UserData;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -19,5 +23,20 @@ public class ApiServiceImpl implements ApiService {
     public List<User> getUsers(Integer limit) {
         UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsersReactive(Mono<Integer> limit) {
+
+        return limit.flatMapMany(integer ->
+                WebClient.create("http://apifaketory.com/api/user?limit=" + limit)
+                        .get()
+                        .uri(uriBuilder -> uriBuilder.queryParam("limit", integer).build())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .exchange()
+                        .flatMap( clientResponse -> clientResponse.bodyToMono(UserData.class))
+                        .flatMapIterable(UserData::getData)
+
+        );
     }
 }
