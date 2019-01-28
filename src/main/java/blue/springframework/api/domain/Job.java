513c3c1e8e0c4933cfda7b7b1493fca2840b3eb2
CyrilenBlu <lukepetzerint2@gmail.com>
2019-01-28 12:09:15 +0200

    Added generated data model with implementation.

--- src/main/java/blue/springframework/api/domain/Job.java
+++ src/main/java/blue/springframework/api/domain/Job.java
@@ -1,6 +1,40 @@
+
 package blue.springframework.api.domain;
 
-public class Job {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Job implements Serializable
+{
+
     private String title;
     private String company;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 2714128235380095465L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
--- src/main/java/blue/springframework/api/domain/Name.java
+++ src/main/java/blue/springframework/api/domain/Name.java
@@ -1,7 +1,49 @@
+
 package blue.springframework.api.domain;
 
-public class Name {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Name implements Serializable
+{
+
     private String title;
     private String first;
     private String last;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = -3978165418976773357L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
